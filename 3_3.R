c7fd88fc64f9217e91f801d6363d3c5c18d93114
bmagalhaes <60456840+bmagalhaes@users.noreply.github.com>
2020-04-16 01:26:54 -0500

    Create 3_3.R

---
+++ 3_3.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(LICORS)
+
+social_raw = read.csv("https://raw.githubusercontent.com/jgscott/ECO395M/master/data/social_marketing.csv")
+
+summary(social)
+length(which(social_raw$spam > 0))
+length(which(social_raw$adult > 0))
+length(which(social_raw$spam > 0 & social_raw$adult > 0))
+
+social_adult = subset(social_raw, spam == 0)
+social_adult$spam = NULL
+
+social_adult = social_adult %>%
+  mutate(adultpct = adult/(select(., chatter:adult) %>% rowSums(na.rm = TRUE)))
+
+length(which(social_adult$adultpct >= 0.25))
+
+x = social[,(2:36)]
+x = scale(x, center = TRUE, scale = TRUE) #subtracting the mean and dividind by the st dev
+
+mu = attr(x,"scaled:center")
+sigma = attr(x,"scaled:scale")
+
+clust1 = kmeanspp(x, 6, nstart = 25)
+
+clust1$centers * sigma + mu
+
+colnames(social)
+qplot(adult, online_gaming, data=social, color=factor(clust1$cluster)) +
+  theme_bw()
+
+# Compare versus within-cluster average distances from the first run
+clust1$withinss
+sum(clust1$withinss)
+clust1$tot.withinss
+clust1$betweenss
